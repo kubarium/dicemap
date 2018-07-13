091e82598333f12dba03ca9bc593e93a8829eb49
kubarium <iam@denizkumsal.com>
2018-07-13 00:08:40 -0400

    introduces buttons with their state broken with the coords

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
 <template>
-  <section class="section">
-    <div id="app" class="container">
-      <nav class="navbar is-transparent">
+  <div id="app" class="container">
+    <nav class="navbar is-fixed-top">
+      <section class="container">
         <div class="navbar-brand">
-          <h1 class="title">Dicemap</h1>
+          <h1 class="title is-3">Dicemap</h1>
           <div class="navbar-burger burger" data-target="navbarExampleTransparentExample">
             <span></span>
             <span></span>
@@ -13,82 +13,72 @@
 
         <div id="navbarExampleTransparentExample" class="navbar-menu">
           <div class="navbar-start">
-            <a class="navbar-item" href="https://bulma.io/">
-              Home
-            </a>
-            <div class="navbar-item has-dropdown is-hoverable">
-              <a class="navbar-link" href="/documentation/overview/start/">
-                Docs
-              </a>
-              <div class="navbar-dropdown is-boxed">
-                <a class="navbar-item" href="/documentation/overview/start/">
-                  Overview
-                </a>
-                <a class="navbar-item" href="https://bulma.io/documentation/modifiers/syntax/">
-                  Modifiers
-                </a>
-                <a class="navbar-item" href="https://bulma.io/documentation/columns/basics/">
-                  Columns
-                </a>
-                <a class="navbar-item" href="https://bulma.io/documentation/layout/container/">
-                  Layout
-                </a>
-                <a class="navbar-item" href="https://bulma.io/documentation/form/general/">
-                  Form
-                </a>
-                <hr class="navbar-divider">
-                <a class="navbar-item" href="https://bulma.io/documentation/elements/box/">
-                  Elements
-                </a>
-                <a class="navbar-item is-active" href="https://bulma.io/documentation/components/breadcrumb/">
-                  Components
-                </a>
-              </div>
-            </div>
+
           </div>
 
           <div class="navbar-end">
             <div class="navbar-item">
-              <p class="control">
-                <a class="button is-primary" href="https://github.com/jgthms/bulma/releases/download/0.7.1/bulma-0.7.1.zip">
-                  <span class="icon">
-                    <i class="fas fa-dice"></i>
-                  </span>
-                  <span>Roll</span>
-                </a>
+              <p class="is-5">
+                Points: {{points.length}}
               </p>
+            </div>
+            <div class="navbar-item">
+              <p class="is-5">
+                Point: {{point.x-offsetX}}-{{point.y-offsetY}}
+              </p>
+            </div>
+            <div class="navbar-item">
+              <div class="field has-addons">
+                <div class="control" v-if="dice.length">
+                  <div class="buttons has-addons">
+                    <span class="button is-small" @click="applyDice">Apply All</span>
+                    <button class="button is-small" :disabled="hasPoint(die)" :class="{'is-link':die.applied}" v-for="(die,index) in dice" :key="index" @click="applyDie(die)">{{die.x}}-{{die.y}}</button>
+                  </div>
+                </div>
+                <p class="control">
+                  <input class="input is-small" type="number" min="1" max="10" v-model="times" placeholder="Text input">
+                </p>
+                <p class="control">
+                  <button class="button is-primary is-small" @click="roll">
+                    <span class="icon">
+                      <i class="fas fa-dice"></i>
+                    </span>
+                    <span>Roll</span>
+                  </button>
+                </p>
+              </div>
             </div>
           </div>
         </div>
-      </nav>
-      <section class="section">{{point.x-offsetX}}-{{point.y-offsetY}}</section>
-      <section class="section">
-        <svg>
-          <defs>
-            <pattern id="smallGrid" width="10" height="10" patternUnits="userSpaceOnUse">
-              <path d="M 10 0 L 0 0 0 10" fill="none" stroke="gray" stroke-width="0.5"></path>
-            </pattern>
-            <pattern id="grid10" width="100" height="100" patternUnits="userSpaceOnUse" x="30" y="30">
-              <rect width="100" height="100" fill="url(#smallGrid)"></rect>
-              <path d="M 100 0 L 0 0 0 100" fill="none" stroke="gray" stroke-width="1"></path>
-            </pattern>
-          </defs>
-
-          <text v-for="(v,index) in new Array(10)" :key="`legend-x-${index}`" :x="`${75+(index*100)}`" :y="offsetY" class="legend">{{index%10}}{{index===0?"":"X"}}</text>
-          <text v-for="(v,index) in new Array(10)" :key="`legend-y-${index}`" :y="`${80+(index*100)}`" x="0" class="legend">{{index%10}}{{index===0?"":"X"}}</text>
-          <text v-for="(v,index) in new Array(100)" :key="`x-${index}`" :x="`${30+(index*10)}`" :y="offsetY+10">{{index%10+1}}</text>
-          <text v-for="(v,index) in new Array(100)" :key="`y-${index}`" :y="`${40+(index*10)}`" :x="offsetX">{{index%10+1}}</text>
-
-          <rect class="point red" :x="point.x" :y="point.y" v-show="preview" />
-
-          <rect class="point blue" v-for="(point,index) in points" :key="index" :x="point.x" :y="point.y" />
-
-          <rect fill="url(#grid10)" height="1000 " width="1000 " x="30" y="30" @click="clickPoint" @mousemove="movePoint" @mouseout="preview=false" />
-        </svg>
       </section>
-      <!-- <router-view/> -->
-    </div>
-  </section>
+    </nav>
+
+    <section class="section">
+      <svg>
+        <defs>
+          <pattern id="smallGrid" width="10" height="10" patternUnits="userSpaceOnUse">
+            <path d="M 10 0 L 0 0 0 10" fill="none" stroke="gray" stroke-width="0.5"></path>
+          </pattern>
+          <pattern id="grid10" width="100" height="100" patternUnits="userSpaceOnUse" x="30" y="30">
+            <rect width="100" height="100" fill="url(#smallGrid)"></rect>
+            <path d="M 100 0 L 0 0 0 100" fill="none" stroke="gray" stroke-width="1"></path>
+          </pattern>
+        </defs>
+
+        <text v-for="(v,index) in new Array(10)" :key="`legend-x-${index}`" :x="`${75+(index*100)}`" :y="offsetY" class="legend">{{index%10}}{{index===0?"":"X"}}</text>
+        <text v-for="(v,index) in new Array(10)" :key="`legend-y-${index}`" :y="`${80+(index*100)}`" x="0" class="legend">{{index%10}}{{index===0?"":"X"}}</text>
+        <text v-for="(v,index) in new Array(100)" :key="`x-${index}`" :x="`${30+(index*10)}`" :y="offsetY+10">{{index%10+1}}</text>
+        <text v-for="(v,index) in new Array(100)" :key="`y-${index}`" :y="`${40+(index*10)}`" :x="offsetX">{{index%10+1}}</text>
+
+        <rect class="point red" :x="point.x" :y="point.y" v-show="preview" />
+
+        <rect class="point blue" v-for="(point,index) in points" :key="index" :x="point.x" :y="point.y" />
+
+        <rect fill="url(#grid10)" height="1000 " width="1000 " x="30" y="30" @click="clickPoint" @mousemove="movePoint" @mouseout="preview=false" />
+      </svg>
+    </section>
+    <!-- <router-view/> -->
+  </div>
 </template>
 <script>
 console.clear();
@@ -99,10 +89,37 @@ export default {
       point: { x: 0, y: 0 },
       points: [],
       offsetX: 20,
-      offsetY: 20
+      offsetY: 20,
+      dice: [],
+      times: 10
     };
   },
   methods: {
+    roll() {
+      let timesToRoll = this.times;
+      this.dice.splice(0);
+
+      do {
+        let die = this.lowestDecimal(
+          Math.floor(Math.random() * 1000) + 10,
+          Math.floor(Math.random() * 1000) + 10
+        );
+
+        die.applied = false;
+
+        this.dice.push(die);
+      } while (--timesToRoll);
+    },
+    applyDie(die) {
+      die.applied = !die.applied;
+      this.clickPoint(null, die);
+    },
+    applyDice() {
+      this.dice.forEach(die => {
+        die.applied = !die.applied;
+        this.clickPoint(null, die);
+      });
+    },
     hasPoint(newPoint) {
       return (
         this.points.filter(
@@ -118,8 +135,14 @@ export default {
     addPoint(newPoint) {
       this.points.push(newPoint);
     },
-    clickPoint() {
-      let point = this.normalizeCoords(event.offsetX, event.offsetY);
+    clickPoint(event, override) {
+      let point = override
+        ? this.lowestDecimal(
+            override.x + this.offsetX,
+            override.y + this.offsetY
+          )
+        : this.lowestDecimal(event.offsetX, event.offsetY);
+      console.log(point);
 
       this.hasPoint(point) ? this.removePoint(point) : this.addPoint(point);
     },
@@ -128,13 +151,13 @@ export default {
 
       Object.assign(
         this.point,
-        this.normalizeCoords(event.offsetX, event.offsetY)
+        this.lowestDecimal(event.offsetX, event.offsetY)
       );
     },
-    normalizeCoords(offsetX, offsetY) {
+    lowestDecimal(x, y) {
       return {
-        x: offsetX - offsetX % 10,
-        y: offsetY - offsetY % 10
+        x: x - x % 10,
+        y: y - y % 10
       };
     }
   }
@@ -148,6 +171,9 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+}
+.navbar {
+  padding: 10px 0;
 }
 svg {
   height: 1030px;
